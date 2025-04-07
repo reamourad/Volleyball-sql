96af1fa7520ae96e7403c7424100d5e3ffcce4ab
Anh Thy Vu <157765416+anhthyvu@users.noreply.github.com>
2025-04-07 19:45:55 -0400

    Display query 16 (#70)

--- queries/query16.php
+++ queries/query16.php
@@ -3,7 +3,49 @@
     require_once '../database.php';
 
     $query = "
-        // Todo: Add your SQL query here
+        SELECT
+            cm.CMN,
+            p.FirstName,
+            p.LastName,
+            FLOOR(DATEDIFF(CURDATE(), p.DateOfBirth)/365) AS age, 
+            p.PhoneNumber,
+            p.Email,
+            l.Name AS LocationName
+        FROM 
+            ClubMember cm
+            JOIN Person p ON cm.PersonID = p.PersonID
+            JOIN Location l ON cm.LocationID = l.LocationID
+            JOIN Payment pay ON cm.CMN = pay.CMN
+        WHERE 
+            -- Member is active
+            pay.MembershipEndDate >= CURDATE()
+            
+            --  has been assigned to at least one formation game session
+            AND EXISTS (
+                SELECT 1
+                FROM Role r
+                JOIN Team t ON r.TeamID = t.TeamID
+                JOIN Session s ON (s.Team1ID = t.TeamID OR s.Team2ID = t.TeamID)
+                WHERE r.CMN = cm.CMN
+                AND s.Type = 'Formation'
+            )
+            
+            -- has never lost a game they played in
+            AND NOT EXISTS (
+                SELECT 1
+                FROM Role r
+                JOIN Team t ON r.TeamID = t.TeamID
+                JOIN Session s ON (s.Team1ID = t.TeamID OR s.Team2ID = t.TeamID)
+                WHERE r.CMN = cm.CMN
+                AND (
+                    (t.TeamID = s.Team1ID AND s.Score1 < s.Score2 AND s.Score1 IS NOT NULL AND s.Score2 IS NOT NULL) 
+                    OR
+                    (t.TeamID = s.Team2ID AND s.Score2 < s.Score1 AND s.Score1 IS NOT NULL AND s.Score2 IS NOT NULL)
+                )
+            )
+        ORDER BY 
+            l.Name ASC, 
+            cm.CMN ASC;
     ";
 
     // Execute the query
@@ -55,23 +97,29 @@
     <!-- Main Section -->
     <main>
         <div class="list-container">
-            <h2>Query 16</h2>
+            <h2>Query 16: List of Undefeated Players</h2>
             <table class="data-table">
                 <thead>
-                    <!-- 
-                        //Todo: display attributes needed
-                        example: 
-                        <th>Attribute 1</th>
-                    -->
+                    <tr>
+                        <th>CMN</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Age</th>
+                        <th>Phone Number</th>
+                        <th>Email</th>
+                        <th>Location Name</th>
+                    </tr>
                 </thead>
                 <tbody>
                     <?php while($row = mysqli_fetch_assoc($result)): ?>
                         <tr>
-                            <!-- 
-                                //Todo: display query dynamically based on the name used in the query
-                                example:
-                                <td><?= htmlspecialchars($row['Attribute1']) ?></td>
-                            -->
+                            <td><?= htmlspecialchars($row['CMN']) ?></td>
+                            <td><?= htmlspecialchars($row['FirstName']) ?></td>
+                            <td><?= htmlspecialchars($row['LastName']) ?></td>
+                            <td><?= htmlspecialchars($row['age']) ?></td>
+                            <td><?= htmlspecialchars($row['PhoneNumber']) ?></td>
+                            <td><?= htmlspecialchars($row['Email']) ?></td>
+                            <td><?= htmlspecialchars($row['LocationName']) ?></td>
                         </tr>
                     <?php endwhile; ?>
                 </tbody>
