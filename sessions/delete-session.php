2b1d74a6f4a611504425a24064ced11c4d1855ca
Anh Thy Vu <anhthyvu26@gmail.com>
2025-04-10 01:39:45 -0400

    Fix typo in delete

--- sessions/delete-session.php
+++ sessions/delete-session.php
@@ -12,7 +12,7 @@
         WHERE SessionID = ?
     ";
     $stmt = mysqli_prepare($conn, $checkQuery);
-    mysqli_stmt_bind_param($stmt, "i", $$sessionID);
+    mysqli_stmt_bind_param($stmt, "i", $sessionID);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $session = mysqli_fetch_assoc($result);
@@ -21,34 +21,6 @@
         header("Location: index.php?id=$sessionID&error=Session not found.");
         exit;
     }
-
-    /* TODO
-    //Send email to the player
-    $emailQuery = "
-        INSERT INTO Email (locationID, recipientID, Subject, Date, First100Chars) VALUES (?, ?, ?, ?, ?)
-    ";
-
-    //Set the email parameters
-    $subject = "Removal from Team";
-    $date = date("Y-m-d H:i:s");
-
-    $sessionQuery = "SELECT TeamName, LocationID FROM Team WHERE TeamID = ?";
-    $sessionStmt = mysqli_prepare($conn, $teamQuery);
-    mysqli_stmt_bind_param($teamStmt, 'i', $teamID);
-    mysqli_stmt_execute($teamStmt);
-    $sessionResult = mysqli_stmt_get_result($teamStmt);
-    $sessionRow = mysqli_fetch_assoc($teamResult);
-    $sessionName = $sessionRow['TeamName'];
-    $locationID = $sessionRow['LocationID'];
-
-    $first100Chars = "You have been removed from the {$teamName} Team. Good luck in your future endeavors.";
-
-    $emailStmt = mysqli_prepare($conn, $emailQuery);
-    mysqli_stmt_bind_param($emailStmt, 'iisss', $locationID, $cmn, $subject, $date, $first100Chars);
-
-    if(!mysqli_stmt_execute($emailStmt)) {
-        throw new Exception("Failed to send email: " . mysqli_error($conn));
-    } */
 
     //Delete the session
     try{
